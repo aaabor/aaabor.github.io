54bd8541af3227111d0fea2d8978d293efe20657
Eric Rolfe <elrolfe@gmail.com>
2023-11-26 18:58:02 -0500

    Added filtering

--- searches/searches.js
+++ searches/searches.js
@@ -1,14 +1,58 @@
 var tabs = document.getElementsByClassName("tab");
 var groups = document.getElementsByClassName("search-group");
+var inputs = document.getElementsByClassName("filter-text");
+var searchBlocks = document.querySelectorAll("#saved-searches .search-block");
+var activeContacts = document.querySelectorAll("#active-contact-searches .contact-block");
+var inactiveContacts = document.querySelectorAll("#inactive-contact-searches .contact-block");
 
 function clearActive() {
     for (let i = 0; i < groups.length; i++) {
         let classNames = tabs[i].getAttribute("class").split(" ").filter(n => n !== "active");
-        tabs[i].setAttribute("class", classNames.join());
+        tabs[i].setAttribute("class", classNames.join(" "));
 
         classNames = groups[i].getAttribute("class").split(" ").filter(n => n !== "active");
-        groups[i].setAttribute("class", classNames.join());
+        groups[i].setAttribute("class", classNames.join(" "));
     }
+}
+
+function updateHighlights(blocks, filter, field) {
+    blocks.forEach(b => {
+        // Get class names without the filtered class
+        let classNames = b.getAttribute("class").split(" ").filter(n => n !== "filtered");
+        let filtered = false;
+
+        let titleDiv = b.getElementsByClassName(field);
+        let inner = titleDiv[0].innerHTML;
+
+        // Remove current highlighting
+        inner = inner.replace(/<\/?strong>/gi, "");
+
+        if (filter.trim() !== "") {
+            // Find instances of the current filter, case insensitive
+            re = new RegExp("(?<!~~:>)" + filter + "(?!<:~~)", "gi");
+
+            filtered = true;
+            let index = inner.search(re);
+            while (index !== -1) {
+                filtered = false;
+                let text = inner.substring(0, index);
+                text += "~~:>";
+                text += inner.substring(index, index + filter.length);
+                text += "<:~~";
+                text += inner.substring(index + filter.length);
+                inner = text;
+                index = inner.search(re);
+            }
+        }
+
+        titleDiv[0].innerHTML = inner.replace(/~~:>/g, "<strong>").replace(/<:~~/g, "</strong>");
+
+        if (filtered) {
+            classNames.push("filtered");
+        }
+
+        b.setAttribute("class", classNames.join(" "));
+    });
 }
 
 function onClick(e) {
@@ -20,6 +64,22 @@ function onClick(e) {
     group.setAttribute("class", group.getAttribute("class") + " active");
 }
 
+function onChange(e) {
+    var name = e.target.getAttribute("name");
+
+    if (name === "filterSearch") {
+        updateHighlights(searchBlocks, e.target.value, "search-title");
+    } else if (name == "filterActive") {
+        updateHighlights(activeContacts, e.target.value, "contact-name");
+    } else if (name == "filterInactive") {
+        updateHighlights(inactiveContacts, e.target.value, "contact-name");
+    }
+}
+
 for (let i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener("click", onClick);
+}
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", onChange);
 }
